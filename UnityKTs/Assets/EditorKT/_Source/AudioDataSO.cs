686f94c9330f89b51bc0f739b0e6eb8442f0d298
Алексей <ix.fenix@mail.ru>
2024-11-21 16:37:26 +0300

    feat: custom editor kt

---
+++ UnityKTs/Assets/EditorKT/_Source/AudioDataSO.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorKT
+{
+    [CreateAssetMenu(fileName = "New AudioDataSO", menuName = "SO/AudioDataSO")]
+    public class AudioDataSO : ScriptableObject
+    {
+#if UNITY_EDITOR
+        private bool _showList;
+        private bool _showLabel;
+#endif
+        [SerializeField] [TextArea] private string message;
+        [SerializeField] private List<AudioDataList> audioData = new();
+        [SerializeField] private string id;
+        [SerializeField] private AudioType audioType;
+        
+        public void D()
+        {
+            var message = "";
+            foreach (var item in audioData)
+            {
+                message += item == null ? "null" : item.Data.Count + "\n";
+            }
+            Debug.Log(message);
+        }
+    }
+
+    [Serializable]
+    public class AudioDataList
+    {
+        [field: SerializeField] public string TypeName { get; private set; }
+        [field: SerializeField] public List<AudioData> Data { get; private set; } = new();
+        
+        public AudioDataList(string typeName)
+        {
+            TypeName = typeName;
+        }
+    }
+    
+    [Serializable]
+    public class AudioData
+    {
+        [field: SerializeField] public AudioClip Clip { get; private set; }
+        [field: SerializeField] public float Volume { get; private set; }
+
+        public AudioData(AudioClip clip, float volume)
+        {
+            Clip = clip;
+            Volume = volume;
+        }
+
+        public void SetClip(AudioClip newClip) => Clip = newClip;
+        public void SetVolume(float newVolume) => Volume = newVolume;
+    }
+
+    public enum AudioType
+    {
+        DANGEROUS = 0,
+        FRIENDLY = 1,
+        NEUTRAL = 2,
+        TEST = 3
+    }
+}
---
+++ UnityKTs/Assets/EditorKT/_Source/Editor/AudioDataEditor.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorKT.Editor
+{
+    [CustomEditor(typeof(AudioDataSO))]
+    public class AudioDataEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _id;
+        private SerializedProperty _message;
+        private SerializedProperty _audioType;
+
+        private FieldInfo _listDrawField;
+        private FieldInfo _messageDrawField;
+        private FieldInfo _listsField;
+
+
+        private AudioDataSO _script;
+
+        public override void OnInspectorGUI()
+        {
+            _script = (AudioDataSO)target;
+
+            SetProperty();
+
+            DrawButtons();
+            DrawMessageField();
+            DrawIdField();
+            DrawAudioTypeField();
+            DrawListsField();
+
+            if (GUILayout.Button("debug"))
+                _script.D();
+
+            serializedObject.ApplyModifiedProperties();
+            EditorUtility.SetDirty(_script);
+        }
+
+        private void DrawListsField()
+        {
+            var saveAsset = false;
+            
+            var oldLists = (List<AudioDataList>)_listsField.GetValue(_script);
+
+            var newLists = new List<AudioDataList>();
+
+            for (var index = 0; index < Enum.GetNames(typeof(AudioType)).Length; index++)
+            {
+                var typeName = Enum.GetNames(typeof(AudioType))[index];
+
+                var item = oldLists.FirstOrDefault(x => x.TypeName == typeName);
+                if(item == null)
+                    saveAsset = true;
+                
+                newLists.Add(item ?? new AudioDataList(typeName));
+            }
+
+            if (saveAsset)
+            {
+                AssetDatabase.SaveAssets();
+                saveAsset = false;
+            }
+
+            _listsField.SetValue(_script, newLists);
+
+            if ((bool)_listDrawField.GetValue(_script))
+            {
+                var clips = newLists.First(x => x.TypeName == _audioType.enumNames[_audioType.enumValueIndex]);
+                for (var index = 0; index < clips.Data.Count; index++)
+                {
+                    EditorGUILayout.BeginHorizontal("Box");
+
+                    var setClip =
+                        EditorGUILayout.ObjectField(GUIContent.none, clips.Data[index].Clip, typeof(AudioDataList),
+                                false,
+                                GUILayout.MaxWidth(345f))
+                            as AudioClip;
+                    clips.Data[index].SetClip(setClip);
+                    GUILayout.Space(5);
+                    
+                    var tempVolume = clips.Data[index].Volume;
+                    
+                    clips.Data[index]
+                        .SetVolume(EditorGUILayout.FloatField(clips.Data[index].Volume, GUILayout.MaxWidth(25f)));
+                    GUILayout.FlexibleSpace();
+                    
+                    if(!Mathf.Approximately(tempVolume, clips.Data[index].Volume))
+                        saveAsset = true;
+
+                    if (GUILayout.Button("X", GUILayout.Height(20)))
+                    {
+                        clips.Data.RemoveAt(index);
+                        saveAsset = true;
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+                }
+                
+                if(saveAsset)
+                    AssetDatabase.SaveAssets();
+
+                var newClip = EditorGUILayout.ObjectField("New Clip", null, typeof(AudioClip), false) as AudioClip;
+
+                if (newClip)
+                {
+                    clips.Data.Add(new AudioData(newClip, 1f));
+                    AssetDatabase.SaveAssets();
+                }
+            }
+        }
+
+        private void DrawIdField()
+        {
+            if ((bool)_messageDrawField.GetValue(_script))
+                EditorGUILayout.PropertyField(_id);
+        }
+
+        private void DrawMessageField()
+        {
+            if ((bool)_messageDrawField.GetValue(_script))
+                EditorGUILayout.PropertyField(_message);
+        }
+
+        private void DrawAudioTypeField()
+        {
+            if (!(bool)_listDrawField.GetValue(_script))
+                return;
+
+            EditorGUILayout.PropertyField(_audioType);
+        }
+
+        private void DrawButtons()
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Labels")) _messageDrawField.SetValue(_script, true);
+            if (GUILayout.Button("List")) _listDrawField.SetValue(_script, true);
+            if (GUILayout.Button("Hide"))
+            {
+                _messageDrawField.SetValue(_script, false);
+                _listDrawField.SetValue(_script, false);
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void SetProperty()
+        {
+            _id = serializedObject.FindProperty("id");
+            _message = serializedObject.FindProperty("message");
+            _audioType = serializedObject.FindProperty("audioType");
+
+            _listDrawField =
+                _script.GetType().GetField("_showList", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            _messageDrawField =
+                _script.GetType().GetField("_showLabel", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            _listsField =
+                _script.GetType().GetField("audioData", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+    }
+}
