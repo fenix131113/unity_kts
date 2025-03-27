e3c96a13d82cb54c3785544dc94f22d758dd2e2b
Алексей <ix.fenix@mail.ru>
2025-03-27 19:11:38 +0300

    tweak: gwent folding

--- UnityKTs/Assets/ISP_GWENT/_Source/Cards/SO/SpecialCardSO.cs
+++ UnityKTs/Assets/ISP_GWENT/_Source/Cards/SO/SpecialCardSO.cs
@@ -1,8 +1,7 @@
 ﻿using ISP_GWENT.Cards.Data;
-using ISP_GWENT.Cards.SO;
 using UnityEngine;
 
-namespace ISP_GWENT.Cards
+namespace ISP_GWENT.Cards.SO
 {
     [CreateAssetMenu(fileName = "Special Card", menuName = "SO/GWENT/Special Card")]
     public class SpecialCardSO : CardSO, ISpecialMoveCard
